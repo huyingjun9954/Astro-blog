ba16313329fb27200c6b4ff27f20b6fc83e3803b
yingjun <missthatgirl26@gmail.com>
2026-08-06 20:04:28 +0800

    添加第一条友链

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -31,6 +31,15 @@ export const friendsConfig: FriendLink[] = [
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
+	{
+		title: "年华",
+		imgurl:
+			"https://tu.520781.xyz/file/blog/1779979945007_a.gif",
+		desc: "Hello,everyone！ 我是年华 一个喜欢折腾的开发者，同时每天库库刷题的初中学生 一个…",
+		siteurl: "https://blog.amamo.top",
+		weight: 10, // 权重，数字越大排序越靠前
+		enabled: true, // 是否启用
+	},
 ];
 
 // 获取启用的友链并进行排序
