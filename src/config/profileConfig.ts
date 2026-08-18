b81d34408f5fc289b149a5e71e9b3749f370344c
yingjun <missthatgirl26@gmail.com>
2026-08-18 10:15:17 +0800

    更新头像

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "/assets/profile/avatar.jpeg",
+	avatar: "https://www.stackmeow.tech/file/1787019249755_20260818101406385.jpeg",
 
 	// 名字
 	name: "StackMeow",
